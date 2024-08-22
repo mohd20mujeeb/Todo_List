7f3a845fefded28dfc1b1844a1c268f5ee0aa3ad
mohd20mujeeb <mohd.20mujeeb@gmail.com>
2024-08-22 19:24:43 +0530

    todo list

---
+++ src/MyComponents/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      This is my component
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem quasi quibusdam quam, quae natus molestiae facilis mollitia aperiam, officiis quia voluptatem, voluptas omnis consequatur ut sequi illo placeat perspiciatis ducimus libero quos.
+      </p>
+    </div>
+  )
+}
+
+
